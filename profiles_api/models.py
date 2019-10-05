edc4e0421fadac166b1401c6ebc2ec33d0055b12
YacobGhebre <jjackgm2@live.com>
2019-10-05 09:02:38 +0000

    intital

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -22,7 +22,8 @@ class UserProfileManager(BaseUserManager):
 
     def create_superuser(self, email, name, password):
         """Create and save super user"""
-
+        user = self.create_user(email, name, password)
+        
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
