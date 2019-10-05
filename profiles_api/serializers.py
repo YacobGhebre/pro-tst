ae3dc965193a19f08ec1f7265bbee288386da66b
Yacob <yacob@ultrates.com>
2019-10-05 12:49:51 +0300

    updt

---
+++ profiles_api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+
+class HelloSerializer(serializers.Serializer):
+    """docstring for HelloSerializer(ser."""
+
+    # def __init__(self, arg):
+    #     super(HelloSerializer(ser, self).__init__()
+    #     self.arg = arg
+    # 
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """docstring for HelloApiView."""
+
+    def get(self, request, format=None):
+        an_apiview = [
+        'Uses HTTP methods as function (get, post, patch, put, delete)',
+        'Is similar to traditional Django View',
+        'Gives you the most control',
+        'Is mapped manually to URLs',
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    # def __init__(self, arg):
+    #     super(HelloApiView, self).__init__()
+    #     self.arg = arg
